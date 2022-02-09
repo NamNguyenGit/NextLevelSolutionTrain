f90eb5fcb97246be8a42632873d85a0603025050
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-09 12:06:11 +0700

    Header Content Section

--- Udemy_courses/travel-website/src/components/Header.js
+++ Udemy_courses/travel-website/src/components/Header.js
@@ -9,6 +9,23 @@ const Header = () => {
             <div className="header__video">
                 <video className="header__video__detail" src={state.video} autoPlay loop muted poster={state.poster}></video>
             </div>
+            <div className="header__contents">
+                <div className="container">
+                    <div className="header__contents__text">
+                        <div className="header__contents__text__child">
+                            <h1 className="header__contents__text__child__h1">
+                                We are travel friends
+                            </h1>
+                            <p className="header__contents__text__child__p">
+                                Come and join us we travel the most famous and beautiful places in the world.
+                            </p>
+                            <div className="header__contents__text__child__link">
+                                <button>Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
