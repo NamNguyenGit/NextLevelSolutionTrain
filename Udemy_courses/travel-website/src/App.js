3aaa0bda56e080c95fc4ac954f45e4dfb0788248
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-09 17:09:30 +0700

    create nav bar

--- Udemy_courses/travel-website/src/App.js
+++ Udemy_courses/travel-website/src/App.js
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import ModelProvider from "./context/providers/ModelProvider";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <ModelProvider>
+        <Nav />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
---
+++ Udemy_courses/travel-website/src/components/Nav.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="navLayer">
+      <div className="nav">
+        <div className="nav__content">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Nav;
