7870ebb677d6ebb47f9ed7307d78048ab161c931
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-09 10:20:09 +0700

    Set Up Pages

--- Udemy_courses/travel-website/src/App.js
+++ Udemy_courses/travel-website/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About}  />
+        <Route path="/about" exact component={About} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
