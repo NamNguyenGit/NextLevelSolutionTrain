c45d087230d7e2d6d0db7fd12c5dd387eacdccf1
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-09 11:26:06 +0700

    Add shape to video

--- Udemy_courses/travel-website/src/components/Header.js
+++ Udemy_courses/travel-website/src/components/Header.js
@@ -1,12 +1,13 @@
 import { useState } from "react"
 const Header = () => {
     const [state] = useState({
-        video:'/assets/videos/header.mp4'
+        video:'/assets/videos/header.mp4',
+        poster:"/assets/images/screen.png"
     }) 
     return(
         <div className="header">
             <div className="header__video">
-                <video src={state.video} autoPlay loop muted></video>
+                <video className="header__video__detail" src={state.video} autoPlay loop muted poster={state.poster}></video>
             </div>
         </div>
     )
