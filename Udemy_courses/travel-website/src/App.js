48be8601e1725c50b09ca45f00120f03219e093f
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-09 10:15:04 +0700

    Route

--- Udemy_courses/travel-website/src/App.js
+++ Udemy_courses/travel-website/src/App.js
@@ -1,10 +1,14 @@
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
   return (
-    <div className="App">
-     <h1>Hello travel website</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About}  />
+      </Switch>
+    </Router>
   );
 }
 
