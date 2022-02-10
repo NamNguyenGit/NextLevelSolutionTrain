edf084ce4f2b9d23bb7f731aaaedd6e00945c37f
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-10 11:15:32 +0700

    Lazy Load Img

--- Udemy_courses/travel-website/src/components/DestinationsList.js
+++ Udemy_courses/travel-website/src/components/DestinationsList.js
@@ -1,9 +1,11 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const DestinationsList = ({ destination }) => {
   return (
     <div className="col-3 p-15">
       <div className="destinations__card">
         <div className="destinations__card__img">
-          <img src={destination.image} alt={destination.name} />
+          <LazyLoadImage src={destination.image} alt={destination.name} />
         </div>
       </div>
     </div>
