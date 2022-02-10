89153a9e71d916eb6763c22c1a3db552f586ba9d
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-10 13:28:07 +0700

    Header Detail Page

--- Udemy_courses/travel-website/src/components/Header.js
+++ Udemy_courses/travel-website/src/components/Header.js
@@ -1,38 +1,46 @@
-import { useState } from "react"
-const Header = ({heading,paragraph,children}) => {
-    const [state] = useState({
-        video:'/assets/videos/header.mp4',
-        poster:"/assets/images/screen.png",
-        logo:'/assets/images/logo.png'
-    }) 
-    return(
-        <div className="header">
-            <div className="container pr">
-                <div className="header__logo">
-                    <img src={state.logo} alt="logo" />
-                </div>
-            </div>
-            <div className="header__video">
-                <video className="header__video__detail" src={state.video} autoPlay loop muted poster={state.poster}></video>
-            </div>
-            <div className="header__contents">
-                <div className="container">
-                    <div className="header__contents__text">
-                        <div className="header__contents__text__child">
-                            <h1 className="header__contents__text__child__h1">
-                               {heading}
-                            </h1>
-                            <p className="header__contents__text__child__p">
-                               {paragraph}
-                            </p>
-                            <div className="header__contents__text__child__link">
-                               {children}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+import { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+const Header = ({ heading, paragraph, children, image }) => {
+  const [state] = useState({
+    video: "/assets/videos/header.mp4",
+    poster: "/assets/images/screen.png",
+    logo: "/assets/images/logo.png",
+  });
+  return (
+    <div className="header">
+      <div className="container pr">
+        <div className="header__logo">
+          <img src={state.logo} alt="logo" />
         </div>
-    )
-}
-export default Header
+      </div>
+      <div className="header__video">
+        {image ? (
+          <LazyLoadImage src={image} alt={image} />
+        ) : (
+          <video
+            className="header__video__detail"
+            src={state.video}
+            autoPlay
+            loop
+            muted
+            poster={state.poster}
+          ></video>
+        )}
+      </div>
+      <div className="header__contents">
+        <div className="container">
+          <div className="header__contents__text">
+            <div className="header__contents__text__child">
+              <h1 className="header__contents__text__child__h1">{heading}</h1>
+              <p className="header__contents__text__child__p">{paragraph}</p>
+              <div className="header__contents__text__child__link">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- Udemy_courses/travel-website/src/pages/Details.js
+++ Udemy_courses/travel-website/src/pages/Details.js
@@ -2,16 +2,26 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { useContext, useEffect } from "react";
 import DestinationsContext from "../context/DestinationsContext";
 import { DETAILS } from "../context/types/DestinationsType";
+import Header from "../components/Header";
+import { Helmet } from "react-helmet-async";
 const Details = () => {
   const { destinationsData, dispatch } = useContext(DestinationsContext);
   console.log(destinationsData.details);
+  const {details} = destinationsData;
   const { id } = useParams();
 
   useEffect(() => {
     dispatch({ type: DETAILS, payload: id });
   }, [id]);
 
-  return <h1>Details {id} </h1>;
+  return (
+    <>
+    <Helmet>
+        <title>{details.name}</title>
+    </Helmet>
+      <Header heading={details.name} image={details.bigImage} ></Header>
+    </>
+  );
 };
 
 export default Details;
