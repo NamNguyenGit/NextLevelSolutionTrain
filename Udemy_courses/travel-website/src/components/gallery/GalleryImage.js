6fc1820d055ccbe1c9ad18255ec51b4ffbbaf3d8
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-11 21:26:32 +0700

    Animate Footer  + gallery

--- Udemy_courses/travel-website/src/components/gallery/GalleryImage.js
+++ Udemy_courses/travel-website/src/components/gallery/GalleryImage.js
@@ -10,7 +10,7 @@ const GalleryImage = ({ glr }) => {
 
   return (
     <div className="col-3">
-      <div className="gallery__image">
+      <div className="gallery__image animation">
         <LazyLoadImage src={glr.image} onClick={() => openLightBox(glr) } alt={glr.image} />
       </div>
     </div>
