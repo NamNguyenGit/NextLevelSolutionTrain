5ddeb8654bb0b76dcd9d1e6df1bee78ee83b3251
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-11 18:26:19 +0700

    Page Container

--- Udemy_courses/travel-website/src/pages/About.js
+++ Udemy_courses/travel-website/src/pages/About.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import PageContainer from "../components/PageContainer";
 const About = () => {
+  const [state] = useState({
+    heading: "about us",
+    pageHeading: "world best travel agency company since 2005",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum tempora voluptate a laborum sed placeat magnam adipisci perspiciatis est illo alias enim, ad modi eos cupiditate officia quas repellat accusamus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum tempora voluptate a laborum sed placeat magnam adipisci perspiciatis est illo alias enim, ad modi eos cupiditate officia quas repellat accusamus.",
+  });
+
   return (
     <>
       <Helmet>
         <title>About page</title>
         <meta name="description" content="travel friends about  page "></meta>
       </Helmet>
-      <h1>About page</h1>
+      <PageContainer  data={state} />
     </>
   );
 };
