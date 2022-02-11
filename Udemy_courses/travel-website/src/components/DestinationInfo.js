5319012917120a943e7dee54bc3f7aff086857f1
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-11 21:37:19 +0700

    Animation for Heading

--- Udemy_courses/travel-website/src/components/DestinationInfo.js
+++ Udemy_courses/travel-website/src/components/DestinationInfo.js
@@ -3,13 +3,13 @@ const DestinationInfo = ({ details }) => {
   return (
     <div className="destinationInfo">
       <div className="container">
-        <h2 className="heading">Overview</h2>
+        <h2 className="heading headingAnimation">Overview</h2>
         <div className="row">
           <div className="col-8">
-            <p className="destinationInfo__p">{details.details}</p>
+            <p className="destinationInfo__p animation">{details.details}</p>
           </div>
         </div>
-        <h2 className="heading">Good to know</h2>
+        <h2 className="heading headingAnimation">Good to know</h2>
         <div className="row">
           <div className="col-8">
             <DestinationDetails title="Language" text={details.language} />
--- Udemy_courses/travel-website/src/context/providers/AnimationProvider.js
+++ Udemy_courses/travel-website/src/context/providers/AnimationProvider.js
@@ -2,17 +2,21 @@ import AnimationContext from "../AnimationContext";
 import { useEffect } from "react";
 const AnimationProvider = ({ children }) => {
   const scrollAnimations = () => {
+    const animation = (elements, className) => {
+      elements.forEach((element) => {
+        const elementPosition = element.getBoundingClientRect().top;
+        const viewPortHeight = window.innerHeight - 150;
+        if (elementPosition < viewPortHeight) {
+          element.classList.add(className);
+        } else {
+          element.classList.remove(className);
+        }
+      });
+    };
     const elements = document.querySelectorAll(".animation");
-
-    elements.forEach((element) => {
-      const elementPosition = element.getBoundingClientRect().top;
-      const viewPortHeight = window.innerHeight - 150;
-      if(elementPosition < viewPortHeight) {
-          element.classList.add('animate');
-      }else {
-        element.classList.remove('animate');
-      }
-    });
+    const headngElements = document.querySelectorAll(".headingAnimation");
+    animation(elements, "animate");
+    animation(headngElements, "headingAnimate");
   };
   useEffect(() => {
     window.addEventListener("scroll", scrollAnimations);
