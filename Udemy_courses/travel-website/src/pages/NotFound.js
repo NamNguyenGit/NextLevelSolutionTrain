50905113417cfd5fb615d7c077ab0dae558dade6
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-11 20:49:14 +0700

    Not Found Page

--- Udemy_courses/travel-website/src/pages/NotFound.js
+++ Udemy_courses/travel-website/src/pages/NotFound.js
@@ -1,12 +1,28 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import Header from "../components/Header";
 const NotFound = () => {
+  const [state] = useState({
+    heading: "Opps, 404",
+    paragraph: "Sorry, the page you are looking for does not exist.",
+    image: "assets/images/404.jpg",
+  });
   return (
     <>
       <Helmet>
         <title>Not Found</title>
         <meta name="description" content="travel friends not found"></meta>
       </Helmet>
-      <h1>Not Found</h1>;
+      <Header
+        heading={state.heading}
+        image={state.image}
+        paragraph={state.paragraph}
+      >
+        <Link to="/" className="btn-default">
+          go to home
+        </Link>
+      </Header>
     </>
   );
 };
